9b228e8d1e9745c85e7878c720af30fb5d5dd85a
Peng Hailin <unisko@gmail.com>
2021-11-24 15:08:20 +0800

    Refactored: move windows related program into an independent package.

--- src/main/java/com/xfoss/learningJava/GameLauncher.java
+++ src/main/java/com/xfoss/learningJava/GameLauncher.java
@@ -1,31 +1,16 @@
 package com.xfoss.learningJava;
 
-import com.sun.jna.Function;
-import com.sun.jna.platform.win32.WinDef.BOOL;
-import com.sun.jna.platform.win32.WinDef.DWORD;
-import com.sun.jna.platform.win32.WinDef.DWORDByReference;
-import com.sun.jna.platform.win32.WinNT.HANDLE;
+import com.xfoss.winUtils.*;
 
 public class GameLauncher {
-    private static void enableWindows10AnsiSupport() {        
-        <!-- 这段代码可以修改 Windows 注册表 -->
-        Function GetStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");       
-        DWORD STD_OUTPUT_HANDLE = new DWORD(-11);       
-        HANDLE hOut = (HANDLE) GetStdHandleFunc.invoke(HANDLE.class, new Object[]{STD_OUTPUT_HANDLE});        
-        DWORDByReference p_dwMode = new DWORDByReference(new DWORD(0));       
-        Function GetConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");       
-        GetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, p_dwMode});        
-        int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;       
-        DWORD dwMode = p_dwMode.getValue();       
-        dwMode.setValue(dwMode.intValue() | ENABLE_VIRTUAL_TERMINAL_PROCESSING);       
-        Function SetConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");       
-        SetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, dwMode});   
-    }
+	
     
     public static void main (String[] args) {
 
-        if (System.getProperty("os.name").startsWith("Windows"))
-            enableWindows10AnsiSupport();
+        if (System.getProperty("os.name").startsWith("Windows")) {
+        	RegEditor ed = new RegEditor();
+        	ed.enableWindows10AnsiSupport();
+        }
 
         GuessGame game = new GuessGame();
         game.startGame();
---
+++ src/main/java/com/xfoss/winUtils/RegEditor.java
@@ -0,0 +1,24 @@
+package com.xfoss.winUtils;
+
+import com.sun.jna.Function;
+import com.sun.jna.platform.win32.WinDef.BOOL;
+import com.sun.jna.platform.win32.WinDef.DWORD;
+import com.sun.jna.platform.win32.WinDef.DWORDByReference;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+
+public class RegEditor {
+    public void enableWindows10AnsiSupport() {        
+        // 这段代码可以修改 Windows 注册表 
+        Function GetStdHandleFunc = Function.getFunction("kernel32", "GetStdHandle");       
+        DWORD STD_OUTPUT_HANDLE = new DWORD(-11);       
+        HANDLE hOut = (HANDLE) GetStdHandleFunc.invoke(HANDLE.class, new Object[]{STD_OUTPUT_HANDLE});        
+        DWORDByReference p_dwMode = new DWORDByReference(new DWORD(0));       
+        Function GetConsoleModeFunc = Function.getFunction("kernel32", "GetConsoleMode");       
+        GetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, p_dwMode});        
+        int ENABLE_VIRTUAL_TERMINAL_PROCESSING = 4;       
+        DWORD dwMode = p_dwMode.getValue();       
+        dwMode.setValue(dwMode.intValue() | ENABLE_VIRTUAL_TERMINAL_PROCESSING);       
+        Function SetConsoleModeFunc = Function.getFunction("kernel32", "SetConsoleMode");       
+        SetConsoleModeFunc.invoke(BOOL.class, new Object[]{hOut, dwMode});   
+    }
+}
