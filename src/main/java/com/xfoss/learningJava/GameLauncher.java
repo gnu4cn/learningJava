1e14413f79410a4b1bf6b142923500ce2e88f13a
Peng Hailin <unisko@gmail.com>
2021-11-24 15:34:44 +0800

    UI improved: catched the exception under cli when a gui app fired.

--- src/main/java/com/xfoss/learningJava/GameLauncher.java
+++ src/main/java/com/xfoss/learningJava/GameLauncher.java
@@ -6,6 +6,10 @@ public class GameLauncher {
 	
     
     public static void main (String[] args) {
+    	String osName = System.getProperty("os.name");
+    	
+    	System.out.format("The OS is %s"
+    			+ "\n--------------------------\n", osName);
 
         if (System.getProperty("os.name").startsWith("Windows")) {
         	RegEditor ed = new RegEditor();
---
+++ src/main/java/com/xfoss/learningJava/GuiDemo.java
@@ -0,0 +1,21 @@
+package com.xfoss.learningJava;
+
+import javax.swing.*;
+
+public class GuiDemo {
+	public static void main (String[] args) {
+		try {
+			JFrame f = new JFrame ();
+			JButton b = new JButton ("点我");
+			
+			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			f.getContentPane().add(b);
+			f.setSize(300, 300);
+			f.setVisible(true);
+		} catch (java.awt.HeadlessException e) {
+			System.out.format("没有显示器，无法运行本程序。\n"
+					+ "错误代码\n"
+					+ "------------------------------\n%s\n", e);
+		}
+	}
+}
