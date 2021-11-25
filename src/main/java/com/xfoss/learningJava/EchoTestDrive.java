a4b5cad3de54cb402e8194791442ec3aff55be34
Peng Hailin <unisko@gmail.com>
2021-11-25 10:07:12 +0800

    Updated

---
+++ src/main/java/com/xfoss/learningJava/EchoTestDrive.java
@@ -0,0 +1,31 @@
+package com.xfoss.learningJava;
+
+public class EchoTestDrive {
+    public static void main (String[] args) {
+        Echo e1 = new Echo ();
+        Echo e2 = new Echo ();
+        
+        int x = 0;
+        while ( x<4 ) {
+        	e1.hello();
+        	++e1.count;
+        	if ( x==3 ) {
+        		++e2.count;
+        	}
+        	if ( x>0 ) {
+        		e2.count += e1.count;
+        	}
+        	System.out.format("x = %s\te1.count = %s\te2.count = %s\n", x, e1.count, e2.count);
+        	++x;
+        }
+        
+        System.out.println(e2.count);
+    }
+}
+
+class Echo {
+    int count = 0;
+    void hello () {
+        System.out.println ("helloooo....");
+    }
+}
