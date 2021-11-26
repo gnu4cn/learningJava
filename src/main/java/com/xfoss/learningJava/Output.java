addb3bb29d9c856ef6f098001bd878cb27d62a2a
Peng Hailin <unisko@gmail.com>
2021-11-26 17:51:47 +0800

    Updated.

--- src/main/java/com/xfoss/learningJava/Output.java
+++ src/main/java/com/xfoss/learningJava/Output.java
@@ -10,6 +10,7 @@ public class Output {
         int y = 7;
         for (int x = 1; x < 8; x++) {
             y++;
+
             if (x > 4) {
                 System.out.format("%s ", ++y);
             }
