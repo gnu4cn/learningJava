7dac8633cfdbaf02de2d70c2d491bb5f1a22da93
Peng Hailin <unisko@gmail.com>
2021-12-24 16:00:49 +0800

    Updated.

--- src/main/java/com/xfoss/learningJava/TestFormats.java
+++ src/main/java/com/xfoss/learningJava/TestFormats.java
@@ -2,6 +2,12 @@ package com.xfoss.learningJava;
 
 public class TestFormats {
     public static void main (String [] args) {
-        System.out.format("%,d\n", 1234567890);
+        System.out.format("The number is: %,d\n", 1234567890);
+                                        // 注意：这里的 "%, d" 和 "%,d" 有区别
+                                        // "%," 和 "d" 之间，只能有两种情况：
+                                        // 1. 什么也没有
+                                        // 2. 一个空格
+                                        // 不能有其他任何字符，包括转义字符
+                                        // 两个空格都不可以 
     }
 }
