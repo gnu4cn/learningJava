1a861d4508651b2a771a8ff76f4448617cd49c03
Peng Hailin <unisko@gmail.com>
2021-12-31 22:58:05 +0800

    Updated.

--- src/main/java/com/xfoss/BeatBox/MusicTest1.java
+++ src/main/java/com/xfoss/BeatBox/MusicTest1.java
@@ -6,14 +6,7 @@ public class MusicTest1 {
     public void play () {
         try {
             Sequencer player = MidiSystem.getSequencer();
-            player.open();
-
             System.out.println("我们就得到了一个‘音序器（Sequencer）’");
-
-            Sequence seq = new Sequence(timing, 4);
-            Track t = seq.createTrack();
-            t.add(myMidiEvent);
-            player.setSequence(seq);
         } catch (MidiUnavailableException ex) {
             System.out.println("Bummer");
         }
