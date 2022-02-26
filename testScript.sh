b6b2e9874fd62f2d32bd5a551168773ffcb820db
Lenny Peng <unisko@gmail.com>
2022-02-26 09:54:10 +0800

    Updated.

--- testScript.sh
+++ testScript.sh
@@ -5,5 +5,5 @@ do
     echo ""
     echo "-----第 $i 遍运行 --------"
     echo ""
-    java -jar ${HOME}/eclipse-workspace/learningJava/build/libs/com.xfoss.learningJava-0.0.1.jar
+    java -jar ${PWD}/build/libs/com.xfoss.learningJava-0.0.1.jar
 done
