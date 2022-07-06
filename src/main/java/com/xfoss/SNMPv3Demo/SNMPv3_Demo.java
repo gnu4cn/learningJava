0047f8cbe710e1c8a20426b17c3cebb96384b18a
Peng Hailin <unisko@gmail.com>
2022-07-06 11:23:48 +0800

    Update.

--- src/main/java/com/xfoss/SNMPv3Demo/SNMPv3_Demo.java
+++ src/main/java/com/xfoss/SNMPv3Demo/SNMPv3_Demo.java
@@ -2,7 +2,7 @@ package com.xfoss.SNMPv3Demo;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Vector;
+// import java.util.Vector;
 
 import org.snmp4j.PDU;
 import org.snmp4j.ScopedPDU;
@@ -29,18 +29,40 @@ import org.snmp4j.util.TableEvent;
 import org.snmp4j.util.TableUtils;
 
 public class SNMPv3_Demo {
+    final static OID[] columnOids = new OID[] {
+        new OID("1.3.6.1.2.1.1.3.0"),
+            new OID("1.3.6.1.2.1.1.5.0"),
+            new OID("1.3.6.1.2.1.33.1.3.3.1.2.1"),
+            new OID("1.3.6.1.2.1.33.1.3.3.1.2.2"),
+            new OID("1.3.6.1.2.1.33.1.3.3.1.2.3"),
+            new OID("1.3.6.1.2.1.33.1.3.3.1.3.1"),
+            new OID("1.3.6.1.2.1.33.1.3.3.1.3.2"),
+            new OID("1.3.6.1.2.1.33.1.3.3.1.3.3"),
+            new OID("1.3.6.1.2.1.33.1.5.1.0"),
+            new OID("1.3.6.1.2.1.33.1.5.3.1.2.1"),
+            new OID("1.3.6.1.2.1.33.1.5.3.1.2.1"),
+            new OID("1.3.6.1.2.1.33.1.5.3.1.2.1")
+    };
+
+    final static String nmsAdmin = System.getenv("SNMP_ADMIN");
+    final static String authKey = System.getenv("SNMP_AUTH_KEY");
+    final static String privKey = System.getenv("SNMP_PRIV_KEY");
+
     public static void main(String[] args) throws IOException, InterruptedException {
 
         OctetString localEngineID = new OctetString(MPv3.createLocalEngineID());
-
         Snmp snmp = new Snmp(new DefaultUdpTransportMapping());
+        //
+        // https://stackoverflow.com/questions/57273315/snmp4j-client-gives-unsupported-security-level
+        //
+        // In SNMP4J 3.x the security protocol AuthSHA (and AuthMD5) are no longer added
+        // by default to the static SecurityProtocols instance because they are now considered as unsafe.
+        SecurityProtocols.getInstance().addAuthenticationProtocol(new AuthSHA());
         USM usm = new USM(SecurityProtocols.getInstance(), localEngineID, 0);
+
         SecurityModels.getInstance().addSecurityModel(usm);
         snmp.listen();
 
-        String nmsAdmin = System.getenv("SNMP_ADMIN");
-        String authKey = System.getenv("SNMP_AUTH_KEY");
-        String privKey = System.getenv("SNMP_PRIV_KEY");
 
         // Add User
         UsmUser user = new UsmUser(
@@ -55,13 +77,14 @@ public class SNMPv3_Demo {
 
         UserTarget target = new UserTarget();
         target.setVersion(SnmpConstants.version3);
-        target.setAddress(new UdpAddress("10.12.10.108/10161"));
+        target.setAddress(new UdpAddress("10.12.10.108/161"));
         target.setSecurityLevel(SecurityLevel.AUTH_PRIV);
         target.setSecurityName(new OctetString(nmsAdmin));
-        target.setTimeout(9000);	//3s
+        target.setTimeout(3000);	//3s
         target.setRetries(3);
 
-        OctetString contextEngineId = new OctetString("0002651100[02]");
+        // OctetString contextEngineId = new OctetString("0002651100[02]");
+        OctetString contextEngineId = new OctetString();
         sendRequest(snmp, createGetPdu(contextEngineId), target);
         snmpWalk(snmp, target, contextEngineId);
     }
@@ -72,8 +95,10 @@ public class SNMPv3_Demo {
         pdu.setContextEngineID(contextEngineId);	//if not set, will be SNMP engine id
         //pdu.setContextName(contextName);  //must be same as SNMP agent
 
-        pdu.add(new VariableBinding(new OID("1.3.6.1.2.1.1.3.0")));	//sysUpTime
-        pdu.add(new VariableBinding(new OID("1.3.6.1.2.1.1.5.0")));	//sysName
+        for ( OID oid: columnOids ){
+            pdu.add(new VariableBinding(oid));	//sysUpTime
+        }
+
         return pdu;
     }
 
@@ -102,10 +127,7 @@ public class SNMPv3_Demo {
                 new MyDefaultPDUFactory(PDU.GETNEXT, //GETNEXT or GETBULK)
                 contextEngineId));
         utils.setMaxNumRowsPerPDU(5);	//only for GETBULK, set max-repetitions, default is 10
-        OID[] columnOids = new OID[] {
-            new OID("1.3.6.1.2.1.1.9.1.2"),	//sysORID
-                new OID("1.3.6.1.2.1.1.9.1.3")	//sysORDescr
-        };
+
         // If not null, all returned rows have an index in a range (lowerBoundIndex, upperBoundIndex]
         List<TableEvent> l = utils.getTable(target, columnOids, new OID("3"), new OID("10"));
         for (TableEvent e : l) {
--- testScript.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for i in {1..20}
-do
-    echo ""
-    echo "-----第 $i 遍运行 --------"
-    echo ""
-    java -jar ${PWD}/build/libs/com.xfoss.learningJava-0.0.1.jar
-done
